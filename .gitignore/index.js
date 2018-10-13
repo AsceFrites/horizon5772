8aae92d30339072b87fcdb493ec87c4aef73e084
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 01:44:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,8 +3,8 @@ const bot = new Discord.Client();
 
 var prefix = ("*")
 
-bot.on('ready') , function() {
-    bot.user.setGame("Command: *Help");
+bot.on('ready', function() {
+    bot.user.setGame("Command: ANGEL T PD");
     console.log("Connectedç");
 });
 
@@ -13,11 +13,33 @@ bot.login("NTAwNzg2MTA1MDYzNTcxNDY2.DqP5Vw.HNZ3_MHRzJ4-MQYlvu07cCRIrZo");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
+        message.channel.sendMessage("Liste des commandes: \n Demandez à Angel pas à moi");
     }
 
-    if (message.content === "Salut"){
-        message.reply("Bien le bonjour. :)");
-        console.log("Commande Salut effectué");
+    if (message.content === "Angel"){
+        message.reply("Angel t pd. :)");
+        console.log("Commande Salut effectué")
+        message.reply;
+    }
+    if (message.content === "Alice"){
+        message.reply("Alice ca glisse @Angel ..");
+        message.reply("Gangbang en maternelle :)");
+        console.log("Commande Alice effectué")
+        message.reply;
+    }
+    if (message.content === "Léa"){
+        message.reply("KIKOUJAP PTDR ESPECE DE ROUSSE :)");
+        console.log("Commande Rousse effectué")
+        message.reply;
+    }
+    if (message.content === "Nyxiore"){
+        message.reply("Lucas tête d'écureuil :)");
+        console.log("Commande lUCAS effectué")
+        message.reply;
+    }
+    if (message.content === "Nylonie"){
+        message.reply("Rageuse Arabe qui se touche avec Cobra :)");
+        console.log("Commande Arabe effectué")
+        message.reply;
     }
 });
