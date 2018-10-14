759816dabe3473cb41f9c71d1c65a3ef98b048e9
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 21:07:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -54,19 +54,6 @@ bot.on('message', message => {
        
      }
     
-  if (message.content == "Sondage") {
-    let args = message.content.split(" ").slice(1);
-    let thingToEcho = args.join(" ")
-    var embed = new Discord.RichEmbed()
-     .setDescription("Sondage")
-     .addField(thingToEcho, "Répondre avec ✔ ou ✖")
-     .setColor("#0b8cf1")
-     .setTimestamp()
-  message.channels.sendEmbed(embed)
-       .then(function (message) {
-            message.react("✔")
-            message.react("✖")
-       }
-}
+ 
 
 });
