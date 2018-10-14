9768dcb192b4ea0b7f26e065e8f061fefe9936a2
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 14:24:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,29 +17,37 @@ bot.on('message', message => {
     }
 
     if (message.content === "Angel"){
-        message.reply("Angel t pd. :)");
+        message.reply("Test 1 :)");
         console.log("Commande Salut effectué")
         message.reply;
     }
-    if (message.content === "Alice"){
-        message.reply("Alice ca glisse @Angel ..");
-        message.reply("Gangbang en maternelle :)");
-        console.log("Commande Alice effectué")
-        message.reply;
+    if (message.content === "Test"){
+        message.channel.sendMessage("Test \n Test2")
     }
-    if (message.content === "Léa"){
-        message.reply("KIKOUJAP PTDR ESPECE DE ROUSSE :)");
-        console.log("Commande Rousse effectué")
-        message.reply;
-    }
-    if (message.content === "Nyxiore"){
-        message.reply("Lucas tête d'écureuil :)");
-        console.log("Commande lUCAS effectué")
-        message.reply;
-    }
-    if (message.content === "Nylonie"){
-        message.reply("Rageuse Arabe qui se touche avec Cobra :)");
-        console.log("Commande Arabe effectué")
-        message.reply;
-    }
+
+    let showHelp = function (msg, args) {
+        let embed = new Discord.RichEmbed()
+          .setTitle(`Liste des commandes`)
+          .setDescription(`${Object.keys(commands).length} commandes sont disponibles`)
+          .setColor("#0b8cf1")
+          .setTimestamp()
+          .setFooter(`Demandé par ${msg.author.username}`, msg.author.avatarURL);
+      
+        for (com in commands) {
+          if (commands[com]["show"]) {
+            embed.addField(commands[com]["name"], commands[com]["desc"] + "\n" + commands[com]["format"]);
+          }
+        }
+      
+        msg.channel.send({
+          embed
+        });
+      }   
+      if (message.content.split(" ")[0] == "*AEmbed"){//EMBED
+        var embed = new Discord.RichEmbed()
+        .setColor("#0b8cf1")
+        .setTitle(message.content.slice("*AEmbed ".length))
+        console.log("Commande test")
+        message.channel.send({embed})
+      }  
 });
