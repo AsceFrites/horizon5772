689ac79126d0f5057948cc20ee5b921b75b42fc1
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 23:49:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,36 +4,36 @@ const bot = new Discord.Client();
 var prefix = ("~")
 
 bot.on('ready', function() {
-    bot.user.setGame("Commandes: ~Help");
+    bot.user.setGame("Commandes: ~help");
     console.log("Connectedç");
 });
 
 bot.login(process.env.Token);
 
 bot.on('message', message => {
-        if (message.content === prefix + "Help"){
-            message.channel.sendMessage("Liste des commandes: \n **~Test** : Tester le bot \n **~AEmbed** : Créer des Embed \n **Hugs** : Faites un câlin ! \n **~Avatar** : Afficher votre avatar \n **~Infodiscord** : Infos à propos d'Aena \n ***Bonne visite sur Aena ! :D***");
+        if (message.content === prefix + "help"){
+            message.channel.sendMessage("Liste des commandes: \n **~test** : Tester le bot \n **~aembed** : Créer des Embed \n **Hugs** : Faites un câlin ! \n **~avatar** : Afficher votre avatar \n **~infodiscord** : Infos à propos d'Aena \n ***Bonne visite sur Aena ! :D***");
         }
     
-        if (message.content === "~Test"){
+        if (message.content === "~test5772"){
             message.reply("test :)");
             console.log("Commande Salut effectué")
             message.reply;
         }
-        if (message.content === "Test"){
-            message.channel.sendMessage("Test \n Test2 effectué")
+        if (message.content === "~test"){
+            message.channel.sendMessage("Test \n Test2 effectué \n ```Bot OK```")
         }
     
          
-          if (message.content.split(" ")[0] == "~AEmbed"){//EMBED
+          if (message.content.split(" ")[0] == "~aembed"){//EMBED
             var embed = new Discord.RichEmbed()
-            .setColor("#0b8cf1")
-            .setTitle(message.content.slice("*AEmbed ".length))
+            .setColor("#febfd2")
+            .setTitle(message.content.slice("~aembed ".length))
             console.log("Commande test")
             message.channel.send({embed})
           }
     
-           if (message.content === '~Avatar') {
+           if (message.content === '~avatar') {
             message.reply(message.author.avatarURL);
           }
           
@@ -41,7 +41,7 @@ bot.on('message', message => {
              message.channel.sendMessage("***Tiens un câlin !*** :D");
         }
         
-       if(message.content === "~Infodiscord") { 
+       if(message.content === "~infodiscord") { 
              var embed = new Discord.RichEmbed()
              .setDescription("Information du Discord")
              .addField("Nom du discord", message.guild.name)
