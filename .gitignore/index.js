f5309d8a33d9843610a1425f540a37542fe175e6
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 17:12:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setGame("Command: ANGEL");
+    bot.user.setGame("Command: ~Help");
     console.log("Connectedç");
 });
 
@@ -12,12 +12,12 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n Demandez à Angel pas à moi");
+    if (message.content === prefix + "~Help"){
+        message.channel.sendMessage("Liste des commandes: \n ~Test : Tester le bot \n ~AEmbed : Créer des embed \~Avatar : Afficher votre avatar");
     }
 
-    if (message.content === "Angel"){
-        message.reply("Angel test :)");
+    if (message.content === "~Test"){
+        message.reply("test :)");
         console.log("Commande Salut effectué")
         message.reply;
     }
@@ -26,7 +26,7 @@ bot.on('message', message => {
     }
 
      
-      if (message.content.split(" ")[0] == "*AEmbed"){//EMBED
+      if (message.content.split(" ")[0] == "~AEmbed"){//EMBED
         var embed = new Discord.RichEmbed()
         .setColor("#0b8cf1")
         .setTitle(message.content.slice("*AEmbed ".length))
@@ -34,7 +34,7 @@ bot.on('message', message => {
         message.channel.send({embed})
       }
 
-       if (message.content === 'AVATAR') {
+       if (message.content === '~avatar') {
         message.reply(message.author.avatarURL);
       }
       
