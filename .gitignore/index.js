d887d85d716621ca236a151897ccb54190c6bb04
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 19:18:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -41,5 +41,16 @@ bot.on('message', message => {
     if (message.content == "Hugs") { 
          message.channel.sendMessage("***Tiens un câlin !*** :D");
     }
+    
+    if(message.content === "infodiscord) {
+var embed= new Discord.RichEmbed()
+.setDescription("Information du Discord")
+.addField("Nom du discord", message.guild.name)
+.addField("Crée le", message.guild.createdAt)
+.addField("Tu as rejoins le", message.member.joinedAt)
+.addField("Utilisateur sur le discord", message.guild.memberCount)
+.setColor("0x0000FF")
+message.channel.sendEmbed(embed)
+}
 
 });
