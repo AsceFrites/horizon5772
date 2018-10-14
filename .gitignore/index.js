70dc5c6b0b53fb6e1a343d0f47392b728720e364
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-14 15:26:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,12 +34,5 @@ bot.on('message', message => {
         message.channel.send({embed})
       }
  
- client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(général => général.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
  
 });
