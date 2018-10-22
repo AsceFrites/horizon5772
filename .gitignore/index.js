f9705213ed64e2e8144d00d77d9240a0f6cf3167
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-22 23:45:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -53,33 +53,6 @@ bot.on('message', message => {
            
          }
 
-         if (message.content.startsWith('~play')) {
-            // On récupère le premier channel audio du serveur
-            let voiceChannel = message.guild.channels
-              .filter(function (channel) { return channel.type === 'voice' })
-              .first()
-            // On récupère les arguments de la commande 
-            // il faudrait utiliser une expression régulière pour valider le lien youtube
-            let args = message.content.split(' ')
-            // On rejoint le channel audio
-            voiceChannel
-              .join(Musique 🎵)
-              .then(function (connection) {
-                // On démarre un stream à partir de la vidéo youtube
-                let stream = YoutubeStream(args[1])
-                stream.on('error', function () {
-                  message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-                  connection.disconnect()
-                })
-                // On envoie le stream au channel audio
-                // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-                connection
-                  .playStream(stream)
-                  .on('end', function () {
-                    connection.disconnect()
-                  })
-              })
-          }
-        
-    
+         
+       
     });
