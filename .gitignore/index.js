e16a735f8523af4f37b0712bc3098c040102c943
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-10-31 11:10:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -83,7 +83,7 @@ bot.on('message', message => {
           console.log("sondage")
           message.channel.send({embed}).then(embedMessage => {
             embedMessage.react("👍");
-            embedMessage.react(":thumbsdown:");
+            embedMessage.react("👎");
 
 
           })
