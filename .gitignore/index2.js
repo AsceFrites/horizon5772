6fabb5f9fda1ac149a47f09a2c85e2e53d0b77c9
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-11-01 10:33:04 +0100

    Update index2.js

--- .gitignore/index2.js
+++ .gitignore/index2.js
@@ -1,1 +1,17 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
+var prefix = ("~")
+
+bot.on('ready', function() {
+    bot.user.setGame("Commandes: ~help");
+    console.log("Connectedç");
+});
+
+bot.login(process.env.Token);
+
+bot.on('message', message => {
+        if (message.content === prefix + "Index"){
+            message.channel.sendMessage("Index2 !");
+        }
+});
